854f4ddb7ca5e4d83b8fb8b0a0f079287b0b6ae7
rana <ranabdulsalam@gmail.com>
2025-08-07 04:11:16 +0530

    Fix

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,3 @@
-
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
@@ -10,7 +9,13 @@ const nextConfig: NextConfig = {
       },
     },
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
-
